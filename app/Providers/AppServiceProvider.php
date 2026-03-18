ccfbc3d04673a3e303ce000a1f0b485f73067105
izzan <izzanahza5@gmail.com>
2026-03-19 00:21:19 +0700

    vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,5 +13,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap(); // <--- Tambahkan ini
+        if (config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
     }
 }
